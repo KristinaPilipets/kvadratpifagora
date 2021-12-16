da86cb4d248a5bcd243257f4956a63efdcd3153b
opilane <opilane@HP-CZC6297BB1>
2021-12-16 12:29:33 +0200

    kvadratpifagora

--- kvadratpifagora.py
+++ kvadratpifagora.py
@@ -1,9 +1,9 @@
 from modul import*
 a=input("Siseta sinu sünnidaatum (pp.kk.aaaa) >>> ")
-if len(a)==12:
-	ans=kntrool(a)
-	if ans=="õige":
-		pifagor()
+if len(a)==10:
+	ans=kontrool(a)
+	if ans==True:
+		pifagor(a)
 	else:
 		print("Vale sünnidaatum proovi uuesti")
 else:
--- modul.py
+++ modul.py
@@ -1,18 +1,39 @@
-def kontrool(a)->str:
-	if a[0:2].isdigit()==True and a[3:5].isdigit()==True and a[6:13].isdigit()==True:
-		if int(a[0:2]) <=31 and int(a[3:5])<=12 and int(a[6:13]):
-			if int(a[3:5])==2:
-				if int(a[6:13])<=29:
-					ans="õige"
-				else:
-					pass
-			else:
-				ans="õige"
+from datetime import*
+def kontrool(a)->bool:
+	if a[0:2].isdigit()==True and a[3:5].isdigit()==True and a[6:10].isdigit()==True:
+		d=int(a[0:2])
+		m=int(a[3:5])
+		y=int(a[6:10])
+		print(d,m,y)
+		if date(y,m,d) is not None:
+			data=True
 		else:
-			ans="vale"
+			data=False
 	else:
-		ans="vale"
-	return ans
+		data=False
+	return data
 
 def pifagor(a):
-	pass
+	d=a[0:2]
+	m=a[3:5]
+	y=a[6:10]
+	dy=int(a[0])+int(a[1])+int(a[3])+int(a[4])
+	year=int(a[7])+int(a[8])+int(a[9])+int(a[-1])
+	first=dy+year
+	first=str(first)
+	second=int(first[0])+int(first[1])
+	third=int(first)-2*int(a[0:2])
+	third=str(abs(third))
+	fourth=int(third[0])+int(third[-1])
+	firstrow=(d+m+y)
+	secondrow=(str(first)+str(second)+str(third)+str(fourth))
+	listid=list(firstrow+secondrow)
+	listid=[int(x) for x in listid]
+	print(listid)
+	count={}
+	for i in range(1,10):
+		a=listid.count(i)
+		if a==0:
+			a="нет"
+		count.update({i:a})
+	print(count.items())
